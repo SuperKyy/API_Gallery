85db1e67fc25c706b1ce957701f0acc3f25ca31d
SuperKyy <139345547+SuperKyy@users.noreply.github.com>
2024-01-23 13:55:11 +0700

    second commit

--- myweb/apps/dashboard/models.py
+++ myweb/apps/dashboard/models.py
@@ -5,12 +5,12 @@ from uuid import uuid4
 def file_image(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (str(uuid4()), ext)
-    return os.path.join('upload', filename)
+    return os.path.join('static/img', filename)
 
 # Create your models here.
 class Gallery(models.Model):
     Judul = models.CharField(max_length=100)
     Gambar = models.ImageField(upload_to=file_image, verbose_name='Photo', null=True, blank=True)
-    Deskripsi = models.TextField()
+    Deskripsi = models.TextField() 
     def __str__(self):
         return self.Judul
--- myweb/myweb/urls.py
+++ myweb/myweb/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +26,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
+
+urlpatterns += staticfiles_urlpatterns()
